22467986ab969e35549da79c988295d921cbaa71
PH_ JAYSON <enverzojayson13@gmail.com>
2025-10-06 00:15:31 +0800

    Using Loop and If-Else

--- LINQandLAMBDA/Program.cs
+++ LINQandLAMBDA/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace LINQAndLAMBDA
+{
+    class Program
+    {
+       public static void Main (string[] args)
+        {
+            var numbers = new[] { 5, 6, 2, 9, 1 };
+
+            //Using Loop and If-else
+            var evenNumbers = new List<int>();
+            foreach (var number in numbers)
+            {
+                if(number  % 2 == 0)
+                {
+                    evenNumbers.Add(number);
+                }
+            }
+            Console.WriteLine("Foreach and If");
+           foreach (var even in numbers)
+            {
+                Console.WriteLine(even);
+            }
+
+            Console.WriteLine("******************");
+            Console.WriteLine("LINQ");
+
+            //using LINQ
+            var linqEvenNumbers = from number in numbers where number % 2 == 0 select number;
+            foreach (var even in linqEvenNumbers) ;
+            Console.WriteLine("even");
+        }
+    }
+}
